97f7246ba229f2de2d7262ae8f553a46b4aa6bd4
D-Jy <duan@d-jy.net>
2024-03-31 03:35:17 +0800

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,38 @@
+FROM phusion/baseimage:jammy-1.0.1 as builder
+
+MAINTAINER D-Jy <duan@d-jy.net>
+
+# Use baseimage-docker's init system.
+CMD ["/sbin/my_init"]
+
+RUN echo 'APT::Get::Clean=always;' >> /etc/apt/apt.conf.d/99AutomaticClean
+
+RUN apt-get update -qqy \
+    && DEBIAN_FRONTEND=noninteractive apt-get -qyy install \
+	--no-install-recommends \
+	build-essential \
+	git \
+	cmake \
+	fakeroot \
+    && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+WORKDIR /tmp
+
+RUN git clone --recursive https://github.com/rozhuk-im/msd_lite.git
+RUN cd msd_lite && mkdir build && cd build && cmake .. &&  make -j 8 && make install
+
+
+FROM debian:stable
+
+COPY --from=builder /usr/local/bin/msd_lite /usr/local/bin/msd_lite
+COPY --from=builder /usr/local/etc/msd_lite/msd_lite.conf.sample /usr/local/etc/msd_lite/msd_lite.conf.sample
+COPY msd_lite.conf /root/msd_lite.conf
+COPY msd_lite.sh /root/msd_lite.sh
+RUN chmod +x /root/msd_lite.sh
+RUN mkdir /etc/msd_lite
+
+WORKDIR /root
+
+ENTRYPOINT ["/bin/bash", "/root/msd_lite.sh"]
+#CMD ["eth0", "7088"]
+CMD []
---
+++ msd_lite.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Read network interface name and port from environment variables or command line arguments
+ifName=${IFNAME:-$1}
+port=${PORT:-$2}
+
+# Check if the msd_lite configuration file exists
+if [ -f /etc/msd_lite/msd_lite.conf ]; then
+    # If the configuration file exists, run msd_lite with it
+    /usr/local/bin/msd_lite -c /etc/msd_lite/msd_lite.conf
+else
+    # If not, copy the sample configuration file to the directory
+    cp /usr/local/etc/msd_lite/msd_lite.conf.sample /etc/msd_lite/msd_lite.conf.sample
+fi
+
+# Exit if no network interface name is provided
+if [ -z "$ifName" ]; then
+    echo "Please modify /etc/msd_lite/msd_lite.conf.sample and rename it to msd_lite.conf. Exiting."
+    exit 1
+fi
+
+# Copy the configuration file from root to the correct location
+cp /root/msd_lite.conf /etc/msd_lite/msd_lite.conf
+
+# Replace the placeholder with the actual network interface name
+sed -i "s/@ifName@/$ifName/g" /etc/msd_lite/msd_lite.conf
+
+# If a port is provided, replace the default port with it
+if [ ! -z "$port" ]; then
+    sed -i "s/7088/$port/g" /etc/msd_lite/msd_lite.conf
+fi
+
+# Execute msd_lite with the configuration file
+/usr/local/bin/msd_lite -c /etc/msd_lite/msd_lite.conf
